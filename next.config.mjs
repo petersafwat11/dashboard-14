722ae98ff905d79ef330cf14a25321519cb485c6
peter safwat <petersafwat640@gmail.com>
2024-09-28 20:24:41 +0300

    added vercel links as the backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    FRONTEND_SERVER: "http://localhost:3000",
-    BACKEND_SERVER: "http://localhost:5000/api",
-    STATIC_SERVER: "http://localhost:5000",
+    FRONTEND_SERVER: "https://vercel-backend-steel-two.vercel.app",
+    BACKEND_SERVER: "https://vercel-backend-steel-two.vercel.app/api",
+    STATIC_SERVER: "https://vercel-backend-steel-two.vercel.app",
+    // FRONTEND_SERVER: "http://localhost:3000",
+    // BACKEND_SERVER: "http://localhost:5000/api",
+    // STATIC_SERVER: "http://localhost:5000",
+    
     // FRONTEND_SERVER: "https://ajsports.ch",
     // BACKEND_SERVER: "https://ajsports.ch/api",
     // STATIC_SERVER: "https://ajsports.ch",
