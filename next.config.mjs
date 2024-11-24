bbedf02d2f00828cacdaf20b9519dece536b9d90
peter safwat <petersafwat640@gmail.com>
2024-11-24 21:00:23 +0200

    switch servers

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {
   
   env: {
-    // FRONTEND_SERVER: "https://al-backend-production.up.railway.app",
-    // BACKEND_SERVER: "https://al-backend-production.up.railway.app/api",
-    // STATIC_SERVER: "https://al-backend-production.up.railway.app",
-    FRONTEND_SERVER: "http://localhost:3000",
-    BACKEND_SERVER: "http://localhost:5000/api",
-    STATIC_SERVER: "http://localhost:5000",
+    FRONTEND_SERVER: "https://al-backend-production.up.railway.app",
+    BACKEND_SERVER: "https://al-backend-production.up.railway.app/api",
+    STATIC_SERVER: "https://al-backend-production.up.railway.app",
+    // FRONTEND_SERVER: "http://localhost:3000",
+    // BACKEND_SERVER: "http://localhost:5000/api",
+    // STATIC_SERVER: "http://localhost:5000",
 
     // FRONTEND_SERVER: "https://ajsports.ch",
     // BACKEND_SERVER: "https://ajsports.ch/api",
